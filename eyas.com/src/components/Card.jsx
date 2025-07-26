bfb7857bedf922b775457357765565b90e0283d8
Eyas <148169791+Eyas-OB@users.noreply.github.com>
2025-07-25 19:18:12 -0500

    Refactor Card component export

--- eyas.com/src/components/Card.jsx
+++ eyas.com/src/components/Card.jsx
@@ -5,11 +5,13 @@
 import React from 'react';
 
 // Defining a functional component called Card
-const Card = () => {
+export default function Card() {
   // This function returns JSX (like HTML in JavaScript)
   return (
     // A div with inline styles to make it look like a card
-    <div style={{ border: '1px solid #ccc', padding: '16px', borderRadius: '8px' }}>
+    <div
+      style={{ border: '1px solid #ccc', padding: '16px', borderRadius: '8px' }}
+    >
       {/* This is the title of the card */}
       <h2>Card Title</h2>
 
@@ -17,7 +19,4 @@ const Card = () => {
       <p>This is the card content.</p>
     </div>
   );
-};
-
-// Exporting the Card component so it can be used in other files (like App.jsx)
-export default Card;
+}
